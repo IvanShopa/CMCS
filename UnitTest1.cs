01526d4bdaa58e0094dcdb2dd84ed06a071407ea
IvSh <80276965+IvanShopa@users.noreply.github.com>
2024-11-01 15:03:14 +0200

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,31 +10,17 @@ namespace UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-      String Message = " clear  Display :  -0, 128, 255.";
+      String Message = "  clear  Display :  0, 128, 255.    ";
 
-      Program.Command Command1 = Program.Function(Message);
+      Program.Command command = Program.Function(Message);
 
       String Result1 = "";
-      if (Command1 is Program.ClearDisplay ClearDisplay2)
+      if (command is Program.ClearDisplay clearDisplay)
       {
-        Result1 = "Result:" +
-                  "\nCommand: " + ClearDisplay2.Name + ".\n" +
-                  "Parameter:\n" +
-                  "Red = " + ClearDisplay2.Color1.Red + ";\n" +
-                  "Green = " + ClearDisplay2.Color1.Green + ";\n" +
-                  "Blue = " + ClearDisplay2.Color1.Blue + ".\n\n";
-      }
-      else if (Command1 is Program.Error Error2)
-      {
-        Result1 = "Result:" + Error2.Text;
+        Result1 = clearDisplay.Name;
       }
 
-      String Result2 = "Result:" +
-                       "\nCommand: Clear Display.\n" +
-                       "Parameter:\n" +
-                       "Red = 0;\n" +
-                       "Green = 128;\n" +
-                       "Blue = 255.\n\n";
+      String Result2 = "clear display";
 
       Assert.AreEqual(Result2, Result1);
     }
@@ -42,34 +28,55 @@ namespace UnitTest
     [TestMethod]
     public void TestMethod2()
     {
-      string Message = " clear  Display :  0, 128, 255.";
+      String Message = "  clear  Display :  0, 128, 255.    ";
 
-      Program.Command Command1 = Program.Function(Message);
+      Program.Command command = Program.Function(Message);
 
       String Result1 = "";
-      if (Command1 is Program.ClearDisplay ClearDisplay2)
+      if (command is Program.ClearDisplay clearDisplay)
       {
-        Result1 = "Result:" +
-                  "\nCommand: " + ClearDisplay2.Name + ".\n" +
-                  "Parameter:\n" +
-                  "Red = " + ClearDisplay2.Color1.Red + ";\n" +
-                  "Green = " + ClearDisplay2.Color1.Green + ";\n" +
-                  "Blue = " + ClearDisplay2.Color1.Blue + ".\n\n";
-      }
-      else if (Command1 is Program.Error Error2)
-      {
-        Result1 = "Result:" + Error2.Text;
+        Result1 = clearDisplay.color.Red.ToString();
       }
 
-      String Result2 = "Result:" +
-                       "\nCommand: Clear Display.\n" +
-                       "Parameter:\n" +
-                       "Red = 0;\n" +
-                       "Green = 128;\n" +
-                       "Blue = 255.\n\n";
+      String Result2 = "0";
 
       Assert.AreEqual(Result2, Result1);
     }
 
+    [TestMethod]
+    public void TestMethod3()
+    {
+      String Message = "  clear  Display :  0, 128, 255.    ";
+
+      Program.Command command = Program.Function(Message);
+
+      String Result1 = "";
+      if (command is Program.ClearDisplay clearDisplay)
+      {
+        Result1 = clearDisplay.color.Green.ToString();
+      }
+
+      String Result2 = "128";
+
+      Assert.AreEqual(Result2, Result1);
+    }
+
+    [TestMethod]
+    public void TestMethod4()
+    {
+      String Message = "  clear  Display :  0, 128, 255.    ";
+
+      Program.Command command = Program.Function(Message);
+
+      String Result1 = "";
+      if (command is Program.ClearDisplay clearDisplay)
+      {
+        Result1 = clearDisplay.color.Blue.ToString();
+      }
+
+      String Result2 = "255";
+
+      Assert.AreEqual(Result2, Result1);
+    }
   }
 }
