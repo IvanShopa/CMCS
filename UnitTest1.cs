31893ba3fd2b92b7da97f913baf04da2f95d3c72
IvSh <80276965+IvanShopa@users.noreply.github.com>
2024-11-02 22:36:25 +0200

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -24,12 +24,9 @@ namespace UnitTest
     public void TestMethod2()
     {
       String Message = "  clear  Display :  -0, 128, 255.    ";
-      Program.ClearDisplay command = (ClearDisplay)Program.Function(Message);
+      Program.Error error = (Error)Program.Function(Message);
 
-      Assert.AreEqual("clear display", command.Name);
-      Assert.AreEqual(0, command.color.Red);
-      Assert.AreEqual(128, command.color.Green);
-      Assert.AreEqual(255, command.color.Blue);
+      Assert.AreEqual(" Error.\n\n", error.Text);
     }
   }
 }
