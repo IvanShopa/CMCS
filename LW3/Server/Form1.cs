3899ffac0e6fb025f43658614d319a05d165e610
IvSh <80276965+IvanShopa@users.noreply.github.com>
2024-12-04 14:03:26 +0200

    Update Form1.cs

--- LW3/Server/Form1.cs
+++ LW3/Server/Form1.cs
@@ -5,6 +5,9 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System;
 
 using static Server.BitmapAndGraphics;
 using static Server.ClassCommand;
@@ -23,7 +26,7 @@ namespace Server
       {
         Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-        IPEndPoint ServerIPEndPoint = new IPEndPoint(IPAddress.Parse("_._._._"), 1);
+        IPEndPoint ServerIPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1);
         EndPoint ServerEndPoint = (EndPoint)ServerIPEndPoint;
 
         ServerSocket.Bind(ServerEndPoint);
@@ -49,6 +52,13 @@ namespace Server
                             "Red = " + ClearDisplay2.color.Red + ";\n" +
                             "Green = " + ClearDisplay2.color.Green + ";\n" +
                             "Blue = " + ClearDisplay2.color.Blue + ".\n\n";
+            CanvasGraphics.Clear(
+              System.Drawing.Color.FromArgb(
+                255, ClearDisplay2.color.Red,
+                ClearDisplay2.color.Green,
+                ClearDisplay2.color.Blue
+              )
+            );
           }
           else if (Command1 is DrawPixel DrawPixel2)
           {
@@ -59,6 +69,12 @@ namespace Server
                             "Red = " + DrawPixel2.color.Red + ";\n" +
                             "Green = " + DrawPixel2.color.Green + ";\n" +
                             "Blue = " + DrawPixel2.color.Blue + ".\n\n";
+            System.Drawing.Color pixelColor = System.Drawing.Color.FromArgb(
+              255, DrawPixel2.color.Red,
+              DrawPixel2.color.Green,
+              DrawPixel2.color.Blue
+            );
+            Canvas.SetPixel(DrawPixel2.X, DrawPixel2.Y, pixelColor);
           }
           else if (Command1 is DrawLine DrawLine2)
           {
@@ -71,6 +87,14 @@ namespace Server
                             "Red = " + DrawLine2.color.Red + ";\n" +
                             "Green = " + DrawLine2.color.Green + ";\n" +
                             "Blue = " + DrawLine2.color.Blue + ".\n\n";
+            Pen pen = new Pen(
+              System.Drawing.Color.FromArgb(
+                255, DrawLine2.color.Red,
+                DrawLine2.color.Green,
+                DrawLine2.color.Blue
+              )
+            );
+            CanvasGraphics.DrawLine(pen, DrawLine2.X1, DrawLine2.Y1, DrawLine2.X2, DrawLine2.Y2);
           }
           else if (Command1 is DrawRectangle DrawRectangle2)
           {
@@ -83,6 +107,19 @@ namespace Server
                             "Red = " + DrawRectangle2.color.Red + ";\n" +
                             "Green = " + DrawRectangle2.color.Green + ";\n" +
                             "Blue = " + DrawRectangle2.color.Blue + ".\n\n";
+            Pen pen = new Pen(
+              System.Drawing.Color.FromArgb(
+                255, DrawRectangle2.color.Red,
+                DrawRectangle2.color.Green,
+                DrawRectangle2.color.Blue
+              )
+            );
+            CanvasGraphics.DrawRectangle(
+              pen, DrawRectangle2.X,
+              DrawRectangle2.Y,
+              DrawRectangle2.W,
+              DrawRectangle2.H
+            );
           }
           else if (Command1 is FillRectangle FillRectangle2)
           {
@@ -95,6 +132,19 @@ namespace Server
                             "Red = " + FillRectangle2.color.Red + ";\n" +
                             "Green = " + FillRectangle2.color.Green + ";\n" +
                             "Blue = " + FillRectangle2.color.Blue + ".\n\n";
+            CanvasGraphics.FillRectangle(
+              new SolidBrush(
+                System.Drawing.Color.FromArgb(
+                  255, FillRectangle2.color.Red,
+                  FillRectangle2.color.Green,
+                  FillRectangle2.color.Blue
+                )
+              ),
+              FillRectangle2.X,
+              FillRectangle2.Y,
+              FillRectangle2.W,
+              FillRectangle2.H
+            );
           }
           else if (Command1 is DrawEllipse DrawEllipse2)
           {
@@ -107,6 +157,18 @@ namespace Server
                             "Red = " + DrawEllipse2.color.Red + ";\n" +
                             "Green = " + DrawEllipse2.color.Green + ";\n" +
                             "Blue = " + DrawEllipse2.color.Blue + ".\n\n";
+            Pen pen = new Pen(
+              System.Drawing.Color.FromArgb(
+                255, DrawEllipse2.color.Red,
+                DrawEllipse2.color.Green,
+                DrawEllipse2.color.Blue
+              )
+            );
+            CanvasGraphics.DrawEllipse(
+              pen, DrawEllipse2.X - DrawEllipse2.RadiusX,
+              DrawEllipse2.Y - DrawEllipse2.RadiusY,
+              DrawEllipse2.RadiusX * 2, DrawEllipse2.RadiusY * 2
+            );
           }
           else if (Command1 is FillEllipse FillEllipse2)
           {
@@ -119,6 +181,18 @@ namespace Server
                             "Red = " + FillEllipse2.color.Red + ";\n" +
                             "Green = " + FillEllipse2.color.Green + ";\n" +
                             "Blue = " + FillEllipse2.color.Blue + ".\n\n";
+            CanvasGraphics.FillEllipse(
+              new SolidBrush(
+                System.Drawing.Color.FromArgb(
+                  255, FillEllipse2.color.Red,
+                  FillEllipse2.color.Green,
+                  FillEllipse2.color.Blue
+                )
+              ),
+              FillEllipse2.X - FillEllipse2.RadiusX,
+              FillEllipse2.Y - FillEllipse2.RadiusY,
+              FillEllipse2.RadiusX * 2, FillEllipse2.RadiusY * 2
+            );
           }
           else if (Command1 is DrawCircle DrawCircle2)
           {
@@ -130,6 +204,18 @@ namespace Server
                             "Red = " + DrawCircle2.color.Red + ";\n" +
                             "Green = " + DrawCircle2.color.Green + ";\n" +
                             "Blue = " + DrawCircle2.color.Blue + ".\n\n";
+            Pen pen = new Pen(
+              System.Drawing.Color.FromArgb(
+                255, DrawCircle2.color.Red,
+                DrawCircle2.color.Green,
+                DrawCircle2.color.Blue
+              )
+            );
+            CanvasGraphics.DrawEllipse(
+              pen, DrawCircle2.X - DrawCircle2.Radius,
+              DrawCircle2.Y - DrawCircle2.Radius,
+              DrawCircle2.Radius * 2, DrawCircle2.Radius * 2
+            );
           }
           else if (Command1 is FillCircle FillCircle2)
           {
@@ -141,6 +227,18 @@ namespace Server
                             "Red = " + FillCircle2.color.Red + ";\n" +
                             "Green = " + FillCircle2.color.Green + ";\n" +
                             "Blue = " + FillCircle2.color.Blue + ".\n\n";
+            Brush brush = new SolidBrush(
+              System.Drawing.Color.FromArgb(
+                255, FillCircle2.color.Red,
+                FillCircle2.color.Green,
+                FillCircle2.color.Blue
+              )
+            );
+            CanvasGraphics.FillEllipse(
+              brush, FillCircle2.X - FillCircle2.Radius,
+              FillCircle2.Y - FillCircle2.Radius,
+              FillCircle2.Radius * 2, FillCircle2.Radius * 2
+            );
           }
           else if (Command1 is DrawRoundedRectangle DrawRoundedRectangle2)
           {
@@ -154,6 +252,36 @@ namespace Server
                             "Red = " + DrawRoundedRectangle2.color.Red + ";\n" +
                             "Green = " + DrawRoundedRectangle2.color.Green + ";\n" +
                             "Blue = " + DrawRoundedRectangle2.color.Blue + ".\n\n";
+            Pen pen = new Pen(
+              System.Drawing.Color.FromArgb(
+                255, DrawRoundedRectangle2.color.Red,
+                DrawRoundedRectangle2.color.Green,
+                DrawRoundedRectangle2.color.Blue
+              )
+            );
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(
+              DrawRoundedRectangle2.X, DrawRoundedRectangle2.Y,
+              DrawRoundedRectangle2.Radius * 2,
+              DrawRoundedRectangle2.Radius * 2, 180, 90
+            );
+            path.AddArc(
+              DrawRoundedRectangle2.X + DrawRoundedRectangle2.W - DrawRoundedRectangle2.Radius * 2,
+              DrawRoundedRectangle2.Y, DrawRoundedRectangle2.Radius * 2,
+              DrawRoundedRectangle2.Radius * 2, 270, 90
+            );
+            path.AddArc(
+              DrawRoundedRectangle2.X + DrawRoundedRectangle2.W - DrawRoundedRectangle2.Radius * 2,
+              DrawRoundedRectangle2.Y + DrawRoundedRectangle2.H - DrawRoundedRectangle2.Radius * 2,
+              DrawRoundedRectangle2.Radius * 2, DrawRoundedRectangle2.Radius * 2, 0, 90
+            );
+            path.AddArc(
+              DrawRoundedRectangle2.X,
+              DrawRoundedRectangle2.Y + DrawRoundedRectangle2.H - DrawRoundedRectangle2.Radius * 2,
+              DrawRoundedRectangle2.Radius * 2, DrawRoundedRectangle2.Radius * 2, 90, 90
+            );
+            path.CloseFigure();
+            CanvasGraphics.DrawPath(pen, path);
           }
           else if (Command1 is FillRoundedRectangle FillRoundedRectangle2)
           {
@@ -167,6 +295,36 @@ namespace Server
                             "Red = " + FillRoundedRectangle2.color.Red + ";\n" +
                             "Green = " + FillRoundedRectangle2.color.Green + ";\n" +
                             "Blue = " + FillRoundedRectangle2.color.Blue + ".\n\n";
+            SolidBrush brush = new SolidBrush(
+              System.Drawing.Color.FromArgb(
+                255, FillRoundedRectangle2.color.Red,
+                FillRoundedRectangle2.color.Green,
+                FillRoundedRectangle2.color.Blue
+              )
+            );
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(
+              FillRoundedRectangle2.X, FillRoundedRectangle2.Y,
+              FillRoundedRectangle2.Radius * 2,
+              FillRoundedRectangle2.Radius * 2, 180, 90
+            );
+            path.AddArc(
+              FillRoundedRectangle2.X + FillRoundedRectangle2.W - FillRoundedRectangle2.Radius * 2,
+              FillRoundedRectangle2.Y, FillRoundedRectangle2.Radius * 2,
+              FillRoundedRectangle2.Radius * 2, 270, 90
+            );
+            path.AddArc(
+              FillRoundedRectangle2.X + FillRoundedRectangle2.W - FillRoundedRectangle2.Radius * 2,
+              FillRoundedRectangle2.Y + FillRoundedRectangle2.H - FillRoundedRectangle2.Radius * 2,
+              FillRoundedRectangle2.Radius * 2, FillRoundedRectangle2.Radius * 2, 0, 90
+            );
+            path.AddArc(
+              FillRoundedRectangle2.X,
+              FillRoundedRectangle2.Y + FillRoundedRectangle2.H - FillRoundedRectangle2.Radius * 2,
+              FillRoundedRectangle2.Radius * 2, FillRoundedRectangle2.Radius * 2, 90, 90
+            );
+            path.CloseFigure();
+            CanvasGraphics.FillPath(brush, path);
           }
           else if (Command1 is DrawText DrawText2)
           {
@@ -180,6 +338,18 @@ namespace Server
                             "Red = " + DrawText2.color.Red + ";\n" +
                             "Green = " + DrawText2.color.Green + ";\n" +
                             "Blue = " + DrawText2.color.Blue + ".\n\n";
+            Font font = new Font(DrawText2.Font, DrawText2.Length);
+            SolidBrush brush = new SolidBrush(
+              System.Drawing.Color.FromArgb(
+                255, DrawText2.color.Red,
+                DrawText2.color.Green,
+                DrawText2.color.Blue
+              )
+            );
+            CanvasGraphics.DrawString(
+              DrawText2.Text, font, brush,
+              new PointF(DrawText2.X, DrawText2.Y)
+            );
           }
           else if (Command1 is DrawImage DrawImage2)
           {
@@ -190,6 +360,10 @@ namespace Server
                             "W = " + DrawImage2.W + ";\n" +
                             "H = " + DrawImage2.H + ";\n" +
                             "Data = " + DrawImage2.Data + ".\n\n";
+            byte[] imageBytes = Convert.FromBase64String(DrawImage2.Data);
+            MemoryStream ms = new MemoryStream(imageBytes);
+            Image img = Image.FromStream(ms);
+            CanvasGraphics.DrawImage(img, DrawImage2.X, DrawImage2.Y, DrawImage2.W, DrawImage2.H);
           }
           else if (Command1 is Error Error2)
           {
