9d1ea99532ec52479f2b4418951afad7b933f5d5
IvSh <80276965+IvanShopa@users.noreply.github.com>
2024-12-04 14:06:01 +0200

    Update FunctionCommand.cs

--- LW3/Server/FunctionCommand.cs
+++ LW3/Server/FunctionCommand.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Drawing.Drawing2D;
-using System.Drawing;
-using System.IO;
 using System.Text.RegularExpressions;
 
-using static Server.BitmapAndGraphics;
 using static Server.ClassCommand;
 
 namespace Server
@@ -36,9 +32,6 @@ namespace Server
         clearDisplay.color.Red = Byte.Parse(Match1.Groups[1].Value);
         clearDisplay.color.Green = Byte.Parse(Match1.Groups[2].Value);
         clearDisplay.color.Blue = Byte.Parse(Match1.Groups[3].Value);
-
-        CanvasGraphics.Clear(System.Drawing.Color.FromArgb(255, clearDisplay.color.Red, clearDisplay.color.Green, clearDisplay.color.Blue));
-
         return clearDisplay;
       }
       else if ((Match1 = RegexDrawPixel.Match(Message)).Success)
@@ -50,10 +43,6 @@ namespace Server
         drawPixel.color.Red = Byte.Parse(Match1.Groups[3].Value);
         drawPixel.color.Green = Byte.Parse(Match1.Groups[4].Value);
         drawPixel.color.Blue = Byte.Parse(Match1.Groups[5].Value);
-
-        System.Drawing.Color pixelColor = System.Drawing.Color.FromArgb(255, drawPixel.color.Red, drawPixel.color.Green, drawPixel.color.Blue);
-        Canvas.SetPixel(drawPixel.X, drawPixel.Y, pixelColor);
-
         return drawPixel;
       }
       else if ((Match1 = RegexDrawLine.Match(Message)).Success)
@@ -67,10 +56,6 @@ namespace Server
         drawLine.color.Red = Byte.Parse(Match1.Groups[5].Value);
         drawLine.color.Green = Byte.Parse(Match1.Groups[6].Value);
         drawLine.color.Blue = Byte.Parse(Match1.Groups[7].Value);
-
-        Pen pen = new Pen(System.Drawing.Color.FromArgb(255, drawLine.color.Red, drawLine.color.Green, drawLine.color.Blue));
-        CanvasGraphics.DrawLine(pen, drawLine.X1, drawLine.Y1, drawLine.X2, drawLine.Y2);
-
         return drawLine;
       }
       else if ((Match1 = RegexDrawRectangle.Match(Message)).Success)
@@ -84,10 +69,6 @@ namespace Server
         drawRectangle.color.Red = Byte.Parse(Match1.Groups[5].Value);
         drawRectangle.color.Green = Byte.Parse(Match1.Groups[6].Value);
         drawRectangle.color.Blue = Byte.Parse(Match1.Groups[7].Value);
-
-        Pen pen = new Pen(System.Drawing.Color.FromArgb(255, drawRectangle.color.Red, drawRectangle.color.Green, drawRectangle.color.Blue));
-        CanvasGraphics.DrawRectangle(pen, drawRectangle.X, drawRectangle.Y, drawRectangle.W, drawRectangle.H);
-
         return drawRectangle;
       }
       else if ((Match1 = RegexFillRectangle.Match(Message)).Success)
@@ -101,12 +82,6 @@ namespace Server
         fillRectangle.color.Red = Byte.Parse(Match1.Groups[5].Value);
         fillRectangle.color.Green = Byte.Parse(Match1.Groups[6].Value);
         fillRectangle.color.Blue = Byte.Parse(Match1.Groups[7].Value);
-
-        CanvasGraphics.FillRectangle(
-          new SolidBrush(System.Drawing.Color.FromArgb(255, fillRectangle.color.Red, fillRectangle.color.Green, fillRectangle.color.Blue)),
-          fillRectangle.X, fillRectangle.Y, fillRectangle.W, fillRectangle.H
-        );
-
         return fillRectangle;
       }
       else if ((Match1 = RegexDrawEllipse.Match(Message)).Success)
@@ -120,10 +95,6 @@ namespace Server
         drawEllipse.color.Red = Byte.Parse(Match1.Groups[5].Value);
         drawEllipse.color.Green = Byte.Parse(Match1.Groups[6].Value);
         drawEllipse.color.Blue = Byte.Parse(Match1.Groups[7].Value);
-
-        Pen pen = new Pen(System.Drawing.Color.FromArgb(255, drawEllipse.color.Red, drawEllipse.color.Green, drawEllipse.color.Blue));
-        CanvasGraphics.DrawEllipse(pen, drawEllipse.X - drawEllipse.RadiusX, drawEllipse.Y - drawEllipse.RadiusY, drawEllipse.RadiusX * 2, drawEllipse.RadiusY * 2);
-
         return drawEllipse;
       }
       else if ((Match1 = RegexFillEllipse.Match(Message)).Success)
@@ -137,12 +108,6 @@ namespace Server
         fillEllipse.color.Red = Byte.Parse(Match1.Groups[5].Value);
         fillEllipse.color.Green = Byte.Parse(Match1.Groups[6].Value);
         fillEllipse.color.Blue = Byte.Parse(Match1.Groups[7].Value);
-
-        CanvasGraphics.FillEllipse(
-          new SolidBrush(System.Drawing.Color.FromArgb(255, fillEllipse.color.Red, fillEllipse.color.Green, fillEllipse.color.Blue)),
-          fillEllipse.X - fillEllipse.RadiusX, fillEllipse.Y - fillEllipse.RadiusY, fillEllipse.RadiusX * 2, fillEllipse.RadiusY * 2
-        );
-
         return fillEllipse;
       }
       else if ((Match1 = RegexDrawCircle.Match(Message)).Success)
@@ -155,10 +120,6 @@ namespace Server
         drawCircle.color.Red = Byte.Parse(Match1.Groups[4].Value);
         drawCircle.color.Green = Byte.Parse(Match1.Groups[5].Value);
         drawCircle.color.Blue = Byte.Parse(Match1.Groups[6].Value);
-
-        Pen pen = new Pen(System.Drawing.Color.FromArgb(255, drawCircle.color.Red, drawCircle.color.Green, drawCircle.color.Blue));
-        CanvasGraphics.DrawEllipse(pen, drawCircle.X - drawCircle.Radius, drawCircle.Y - drawCircle.Radius, drawCircle.Radius * 2, drawCircle.Radius * 2);
-
         return drawCircle;
       }
       else if ((Match1 = RegexFillCircle.Match(Message)).Success)
@@ -171,10 +132,6 @@ namespace Server
         fillCircle.color.Red = Byte.Parse(Match1.Groups[4].Value);
         fillCircle.color.Green = Byte.Parse(Match1.Groups[5].Value);
         fillCircle.color.Blue = Byte.Parse(Match1.Groups[6].Value);
-
-        Brush brush = new SolidBrush(System.Drawing.Color.FromArgb(255, fillCircle.color.Red, fillCircle.color.Green, fillCircle.color.Blue));
-        CanvasGraphics.FillEllipse(brush, fillCircle.X - fillCircle.Radius, fillCircle.Y - fillCircle.Radius, fillCircle.Radius * 2, fillCircle.Radius * 2);
-
         return fillCircle;
       }
       else if ((Match1 = RegexDrawRoundedRectangle.Match(Message)).Success)
@@ -189,16 +146,6 @@ namespace Server
         drawRoundedRectangle.color.Red = Byte.Parse(Match1.Groups[6].Value);
         drawRoundedRectangle.color.Green = Byte.Parse(Match1.Groups[7].Value);
         drawRoundedRectangle.color.Blue = Byte.Parse(Match1.Groups[8].Value);
-
-        Pen pen = new Pen(System.Drawing.Color.FromArgb(255, drawRoundedRectangle.color.Red, drawRoundedRectangle.color.Green, drawRoundedRectangle.color.Blue));
-        GraphicsPath path = new GraphicsPath();
-        path.AddArc(drawRoundedRectangle.X, drawRoundedRectangle.Y, drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Radius * 2, 180, 90);
-        path.AddArc(drawRoundedRectangle.X + drawRoundedRectangle.W - drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Y, drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Radius * 2, 270, 90);
-        path.AddArc(drawRoundedRectangle.X + drawRoundedRectangle.W - drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Y + drawRoundedRectangle.H - drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Radius * 2, 0, 90);
-        path.AddArc(drawRoundedRectangle.X, drawRoundedRectangle.Y + drawRoundedRectangle.H - drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Radius * 2, drawRoundedRectangle.Radius * 2, 90, 90);
-        path.CloseFigure();
-        CanvasGraphics.DrawPath(pen, path);
-
         return drawRoundedRectangle;
       }
       else if ((Match1 = RegexFillRoundedRectangle.Match(Message)).Success)
@@ -213,16 +160,6 @@ namespace Server
         fillRoundedRectangle.color.Red = Byte.Parse(Match1.Groups[6].Value);
         fillRoundedRectangle.color.Green = Byte.Parse(Match1.Groups[7].Value);
         fillRoundedRectangle.color.Blue = Byte.Parse(Match1.Groups[8].Value);
-
-        SolidBrush brush = new SolidBrush(System.Drawing.Color.FromArgb(255, fillRoundedRectangle.color.Red, fillRoundedRectangle.color.Green, fillRoundedRectangle.color.Blue));
-        GraphicsPath path = new GraphicsPath();
-        path.AddArc(fillRoundedRectangle.X, fillRoundedRectangle.Y, fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Radius * 2, 180, 90);
-        path.AddArc(fillRoundedRectangle.X + fillRoundedRectangle.W - fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Y, fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Radius * 2, 270, 90);
-        path.AddArc(fillRoundedRectangle.X + fillRoundedRectangle.W - fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Y + fillRoundedRectangle.H - fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Radius * 2, 0, 90);
-        path.AddArc(fillRoundedRectangle.X, fillRoundedRectangle.Y + fillRoundedRectangle.H - fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Radius * 2, fillRoundedRectangle.Radius * 2, 90, 90);
-        path.CloseFigure();
-        CanvasGraphics.FillPath(brush, path);
-
         return fillRoundedRectangle;
       }
       else if ((Match1 = RegexDrawText.Match(Message)).Success)
@@ -237,11 +174,6 @@ namespace Server
         drawText.color.Red = Byte.Parse(Match1.Groups[5].Value);
         drawText.color.Green = Byte.Parse(Match1.Groups[6].Value);
         drawText.color.Blue = Byte.Parse(Match1.Groups[7].Value);
-
-        Font font = new Font(drawText.Font, drawText.Length);
-        SolidBrush brush = new SolidBrush(System.Drawing.Color.FromArgb(255, drawText.color.Red, drawText.color.Green, drawText.color.Blue));
-        CanvasGraphics.DrawString(drawText.Text, font, brush, new PointF(drawText.X, drawText.Y));
-
         return drawText;
       }
       else if ((Match1 = RegexDrawImage.Match(Message)).Success)
@@ -253,21 +185,6 @@ namespace Server
         drawImage.W = Int16.Parse(Match1.Groups[3].Value);
         drawImage.H = Int16.Parse(Match1.Groups[4].Value);
         drawImage.Data = Match1.Groups[5].Value;
-
-        try
-        {
-          byte[] imageBytes = Convert.FromBase64String(drawImage.Data);
-          MemoryStream ms = new MemoryStream(imageBytes);
-          Image img = Image.FromStream(ms);
-          CanvasGraphics.DrawImage(img, drawImage.X, drawImage.Y, drawImage.W, drawImage.H);
-        }
-        catch (FormatException ex)
-        {
-          Error error = new Error();
-          error.Text = "Error.\n\n";
-          return error;
-        }
-
         return drawImage;
       }
       else
