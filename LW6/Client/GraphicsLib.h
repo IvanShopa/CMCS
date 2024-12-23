01486de279d60509e660a77da59104b290afe834
IvSh <80276965+IvanShopa@users.noreply.github.com>
2024-12-23 16:05:02 +0200

    Update GraphicsLib.h

--- LW6/Client/GraphicsLib.h
+++ LW6/Client/GraphicsLib.h
@@ -5,23 +5,23 @@
 #define RGB(r, g, b) ((uint16_t)((((r) & 0xF8) << 8) | (((g) & 0xFC) << 3) | (((b) & 0xF8) >> 3)))
 #endif
 uint16_t toRGB565(uint8_t red, uint8_t green, uint8_t blue) {
-	return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3);
+  return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3);
 }
 class GraphicsLib {
 public:
-	GraphicsLib(uint_least16_t w, uint_least16_t h) : width(w), height(h), orientation(0) {}
-	GraphicsLib() : width(0), height(0), orientation(0) {}
-	int_least16_t getWidth() const { return this->width; }
-	int_least16_t getHeight() const { return this->height; }
-	virtual void fillScreen(uint_least16_t color) = 0;
-	virtual void drawPixel(int_least16_t x0, int_least16_t y0, uint_least16_t color) = 0;
-	virtual void drawLine(int_least16_t x0, int_least16_t y0, int_least16_t x1, int_least16_t y1, uint_least16_t color) = 0;
-	virtual void drawRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) = 0;
-	virtual void fillRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) = 0;
-	virtual void drawEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) = 0;
-	virtual void fillEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) = 0;
+  GraphicsLib(uint_least16_t w, uint_least16_t h) : width(w), height(h), orientation(0) {}
+  GraphicsLib() : width(0), height(0), orientation(0) {}
+  int_least16_t getWidth() const { return this->width; }
+  int_least16_t getHeight() const { return this->height; }
+  virtual void fillScreen(uint_least16_t color) = 0;
+  virtual void drawPixel(int_least16_t x0, int_least16_t y0, uint_least16_t color) = 0;
+  virtual void drawLine(int_least16_t x0, int_least16_t y0, int_least16_t x1, int_least16_t y1, uint_least16_t color) = 0;
+  virtual void drawRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) = 0;
+  virtual void fillRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) = 0;
+  virtual void drawEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) = 0;
+  virtual void fillEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) = 0;
 protected:
-	int_least16_t width, height;
-	int_least16_t orientation;
+  int_least16_t width, height;
+  int_least16_t orientation;
 };
 #endif
